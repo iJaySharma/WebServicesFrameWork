70d7fbd52364e13a7a74f630977ce4644ea948dd
iJaySharma <jay.sharma96100@gmail.com>
2023-04-18 01:28:33 +0530

    update

--- WEB-INF/classes/bobby/Thinking/EmployeeService.java
+++ WEB-INF/classes/bobby/Thinking/EmployeeService.java
@@ -1,88 +1,113 @@
 package bobby.Thinking;
 import com.thinking.machines.webrock.annotations.*;
 import com.thinking.machines.webrock.injections.*;
-
-@Path("/employee")
+import java.util.*;
+@Path("/test")
 @InjectApplicationScope
+@InjectSessionScope
+@InjectRequestScope
 public class EmployeeService
 {
+@InjectRequestParameter("code")
+private Integer code;
+@InjectRequestParameter("name")
+private Employee nm;
 private ApplicationScope applicationScope;
+private SessionScope sessionScope;
+private RequestScope requestScope;
+private ApplicationDirectory applicationDirectory;
 public void setApplicationScope(ApplicationScope applicationScope)
 {
 this.applicationScope=applicationScope;
-System.out.println("Application Scope invoked for employee service "+applicationScope);
 }
-
-@AutoWired(name="xyz")
-private Employee firstEmployee;
-@AutoWired(name="pqr")
-private Employee secondEmployee;
-
-@Path("/addEmployee")
+public void setSessionScope(SessionScope sessionScope)
+{
+this.sessionScope=sessionScope;
+}
+public void setRequestScope(RequestScope requestScope)
+{
+this.requestScope=requestScope;
+}
+public void setApplicationDirectory(ApplicationDirectory applicationDirectory)
+{
+this.applicationDirectory=applicationDirectory;
+}
+@AutoWired(name="test")
+private Employee e;
+@Path("/testEmployee")
 @Get
-public void addEmployee()
+public void testEmployee()
 {
-System.out.println("Add Employee Invoked");
+if(e!=null)
+{
+System.out.println(e.getCode()+",,,,,"+e.getName());
 }
-
-@Path("/getFirstEmployee")
-@Forward("/employee/printFirstEmployee")
+else
+{
+System.out.println("Problem");
+}
+}
+@Path("/cartoon")
 @Get
-public void getFirstEmployee()
+public void cartoon()
 {
-Employee emp=new Employee();
-emp.setCode(101);
-emp.setName("JAY");
-this.applicationScope.setAttribute("xyz",emp);
-if(this.applicationScope.getAttribute("xyz")!=null)
-{
-Employee e=(Employee)this.applicationScope.getAttribute("xyz");
-System.out.println(e.getCode());
-System.out.println(e.getName());
+System.out.println("Cartoon invoked");
+if(code!=null) System.out.println(code);
+if(nm!=null) System.out.println(nm);
 }
-System.out.println("Get First Employee Called");
-}
-
-@Path("/printFirstEmployee")
+@Path("/testForArguments")
 @Get
-//@Forward("/index.jsp")
-public void printFirstEmployee()
+public Employee testForArguments(int x,String y,Boolean b,Integer z,Character p,@RequestParameter("salary") int salary,@RequestParameter("code") Integer c,@RequestParameter("name") String name)
 {
-if(this.applicationScope.getAttribute("xyz")!=null)
+System.out.println("Test for arguments invoked");
+Employee e=new Employee();
+e.setCode(c);
+e.setName(name);
+if(code!=null && name!=null)
 {
-Employee e=(Employee)this.applicationScope.getAttribute("xyz");
-//System.out.println(e.getCode());
-System.out.println(e.getName());
+System.out.println("Code "+code+" Name "+name+" Salary "+salary);
 }
-System.out.println("print First Employee Called");
+return e;
 }
-
-@Path("/getSecondEmployee")
+@Path("/doSomething")
 @Get
-public void getThirdEmployee()
+@Forward("/test.jsp")
+public void doSomething(@RequestParameter("xyz") Integer xyz,ApplicationScope as,SessionScope ss,RequestScope rs,ApplicationDirectory ad)
 {
-Employee emp=new Employee();
-emp.setCode(102);
-emp.setName("Rajul");
-this.applicationScope.setAttribute("pqr",emp);
-if(this.applicationScope.getAttribute("pqr")!=null)
+System.out.println(xyz);
+if(as!=null) System.out.println("Application scope is not null");
+if(ss!=null) System.out.println("Session scope is not null");
+if(rs!=null) System.out.println("Request Scope is not null");
+if(ad!=null) System.out.println("Application directory is not null");
+}
+@Path("/testForPost")
+@Post
+public String testForPost(ApplicationScope as,SessionScope ss,RequestScope rs,ApplicationDirectory ad,@RequestParameter("xyz") Employee emp)
 {
-Employee e=(Employee)this.applicationScope.getAttribute("pqr");
-System.out.println(e.getName());
+System.out.println("Test For Post Invoked");
+if(emp!=null) System.out.println(emp.getCode()+","+emp.getName());
+if(as!=null) System.out.println("Application scope is not null");
+if(ss!=null) System.out.println("Session scope is not null");
+if(rs!=null) System.out.println("Request Scope is not null");
+if(ad!=null) System.out.println("Application directory is not null");
+return "Hello from test for Post";
 }
-System.out.println("Get Second Employee Called ");
-}
-
-@Path("/printSecondEmployee")
+@Path("/pathVariable")
 @Get
-@Forward("/index.jsp")
-public void printSecondEmployee()
+public void pathVariable(@PathVariable Integer xx,@PathVariable String yy,@RequestParameter("pqr") int pqr)
 {
-System.out.println("Print Second Employee Invoked");
-if(this.firstEmployee==null)
-{
-System.out.println("First employee is null");
+if(pqr!=0) System.out.println(pqr);
+if(xx!=null) System.out.println(xx);
+if(yy!=null) System.out.println(yy);
 }
-if(this.secondEmployee!=null) System.out.println(this.secondEmployee.getCode()+","+this.secondEmployee.getName());
+@Path("/xyz")
+@Post
+public HashMap<Integer,String> xyz(@PathVariable Integer a,@PathVariable String b,Employee emp)
+{
+System.out.println("Request Arrived For Post");
+HashMap<Integer,String> map=new HashMap<>();
+map.put(a,b);
+map.put(emp.getCode(),emp.getName());
+return map;
 }
 }
